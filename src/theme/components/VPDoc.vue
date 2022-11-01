16a2df9e48ad0b7edec24dd687add0ff6b864ca2
King-of-Infinite-Space <27502578+King-of-Infinite-Space@users.noreply.github.com>
2022-11-01 15:21:00 -0400

    + giscus, ^ doc page css

--- src/theme/components/VPDoc.vue
+++ src/theme/components/VPDoc.vue
@@ -1,63 +1,79 @@
 <script setup lang="ts">
-import { useRoute } from 'vitepress'
-import { computed, provide, ref } from 'vue'
-import { useSidebar } from '../composables/sidebar.js'
-import VPDocAside from './VPDocAside.vue'
-import VPDocFooter from './VPDocFooter.vue'
+import { useRoute, useData } from "vitepress"
+import { computed, provide, ref } from "vue"
+import { useSidebar } from "../composables/sidebar.js"
+import VPDocAside from "./VPDocAside.vue"
+import VPDocFooter from "./VPDocFooter.vue"
+import Giscus from "@giscus/vue"
 
 const route = useRoute()
 const { hasSidebar, hasAside } = useSidebar()
 
 const pageName = computed(() =>
-  route.path.replace(/[./]+/g, '_').replace(/_html$/, '')
+  route.path.replace(/[./]+/g, "_").replace(/_html$/, "")
 )
 
+const { frontmatter } = useData()
+const discussionNumber = frontmatter.value.url.split("/").pop()
+
 const onContentUpdated = ref()
-provide('onContentUpdated', onContentUpdated)
+provide("onContentUpdated", onContentUpdated)
 </script>
 
 <template>
-  <div class="VPDoc" :class="{ 'has-sidebar': hasSidebar, 'has-aside': hasAside }">
+  <div
+    class="VPDoc"
+    :class="{ 'has-sidebar': hasSidebar, 'has-aside': hasAside }"
+  >
+    <div v-if="hasAside" class="aside">
+      <div class="aside-curtain" />
+      <div class="aside-container">
+        <div class="aside-content">
+          <VPDocAside>
+            <template #aside-top>
+              <slot name="aside-top" />
+            </template>
+            <template #aside-bottom>
+              <slot name="aside-bottom" />
+            </template>
+            <template #aside-outline-before>
+              <slot name="aside-outline-before" />
+            </template>
+            <template #aside-outline-after>
+              <slot name="aside-outline-after" />
+            </template>
+            <template #aside-ads-before>
+              <slot name="aside-ads-before" />
+            </template>
+            <template #aside-ads-after>
+              <slot name="aside-ads-after" />
+            </template>
+          </VPDocAside>
+        </div>
+      </div>
+    </div>
     <div class="container">
-      <div v-if="hasAside" class="aside">
-        <div class="aside-curtain" />
-        <div class="aside-container">
-          <div class="aside-content">
-            <VPDocAside>
-              <template #aside-top>
-                <slot name="aside-top" />
-              </template>
-              <template #aside-bottom>
-                <slot name="aside-bottom" />
-              </template>
-              <template #aside-outline-before>
-                <slot name="aside-outline-before" />
-              </template>
-              <template #aside-outline-after>
-                <slot name="aside-outline-after" />
-              </template>
-              <template #aside-ads-before>
-                <slot name="aside-ads-before" />
-              </template>
-              <template #aside-ads-after>
-                <slot name="aside-ads-after" />
-              </template>
-            </VPDocAside>
-          </div>
-        </div>
-      </div>
-
       <div class="content">
-        <div class="content-container">
-          <slot name="doc-before" />
-          <main class="main">
-            <Content class="vp-doc" :class="pageName" :onContentUpdated="onContentUpdated" />
-          </main>
-          <slot name="doc-footer-before" />
-          <VPDocFooter />
-          <slot name="doc-after" />
-        </div>
+        <slot name="doc-before" />
+        <main class="main">
+          <Content
+            class="vp-doc"
+            :class="pageName"
+            :onContentUpdated="onContentUpdated"
+          />
+        </main>
+        <slot name="doc-footer-before" />
+        <VPDocFooter />
+        <slot name="doc-after" />
       </div>
+      <Giscus
+        id="comments-giscus"
+        repo="King-of-Infinite-Space/thoughts"
+        repoId="MDEwOlJlcG9zaXRvcnkxMzkwMTY3MjU="
+        mapping="number"
+        :term="discussionNumber"
+        inputPosition="top"
+      />
     </div>
   </div>
 </template>
@@ -66,73 +82,63 @@ provide('onContentUpdated', onContentUpdated)
 @import url("https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css");
 
 .VPDoc {
-  padding: 32px 24px 96px;
+  padding: 32px 24px;
   width: 100%;
+}
+
+.VPDoc .container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  width: 100%;
+}
+
+.VPDoc .content,
+.VPDoc #comments-giscus {
+  max-width: 752px;
+  padding-bottom: 64px;
+}
+
+.aside {
+  position: fixed;
+  display: none;
+  right: 1%;
+  max-width: 256px;
 }
 
 @media (min-width: 768px) {
   .VPDoc {
-    padding: 48px 32px 128px;
+    padding-top: 48px;
   }
 }
 
 @media (min-width: 960px) {
   .VPDoc {
-    padding: 32px 32px 0;
+    padding: 32px 32px;
   }
 
-  .VPDoc:not(.has-sidebar) .container {
-    display: flex;
-    justify-content: center;
-    max-width: 992px;
-  }
-
-  .VPDoc:not(.has-sidebar) .content {
-    max-width: 752px;
-  }
-}
-
-@media (min-width: 1280px) {
-  .VPDoc .container {
-    display: flex;
-    justify-content: center;
-  }
-
-  .VPDoc .aside {
+  .aside {
     display: block;
   }
 }
 
-@media (min-width: 1440px) {
-  .VPDoc:not(.has-sidebar) .content {
-    max-width: 784px;
+@media (min-width: 1280px) {
+  .aside {
+    right: 5%;
   }
-
-  .VPDoc:not(.has-sidebar) .container {
-    max-width: 1104px;
-  }
-}
-
-.container {
-  margin: 0 auto;
-  width: 100%;
-}
-
-.aside {
-  position: relative;
-  display: none;
-  order: 2;
-  flex-grow: 1;
-  padding-left: 32px;
-  width: 100%;
-  max-width: 256px;
 }
 
 .aside-container {
   position: sticky;
   top: 0;
-  margin-top: calc((var(--vp-nav-height-desktop) + var(--vp-layout-top-height, 0px)) * -1 - 32px);
-  padding-top: calc(var(--vp-nav-height-desktop) + var(--vp-layout-top-height, 0px) + 32px);
+  margin-top: calc(
+    (var(--vp-nav-height-desktop) + var(--vp-layout-top-height, 0px)) * -1 -
+      32px
+  );
+  padding-top: calc(
+    var(--vp-nav-height-desktop) + var(--vp-layout-top-height, 0px) + 32px
+  );
   height: 100vh;
   overflow-x: hidden;
   overflow-y: auto;
@@ -155,35 +161,10 @@ provide('onContentUpdated', onContentUpdated)
 .aside-content {
   display: flex;
   flex-direction: column;
-  min-height: calc(100vh - (var(--vp-nav-height-desktop) + var(--vp-layout-top-height, 0px) + 32px));
+  min-height: calc(
+    100vh -
+      (var(--vp-nav-height-desktop) + var(--vp-layout-top-height, 0px) + 32px)
+  );
   padding-bottom: 32px;
-}
-
-.content {
-  position: relative;
-  margin: 0 auto;
-  width: 100%;
-}
-
-@media (min-width: 960px) {
-  .content {
-    padding: 0 32px 128px;
-  }
-}
-
-@media (min-width: 1280px) {
-  .content {
-    order: 1;
-    margin: 0;
-    min-width: 640px;
-  }
-}
-
-.content-container {
-  margin: 0 auto;
-}
-
-.VPDoc.has-aside .content-container {
-  max-width: 688px;
 }
 </style>
