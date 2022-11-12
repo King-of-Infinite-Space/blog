9f94bbd6735f41559e4de0274cad4990b49dee64
King-of-Infinite-Space <27502578+King-of-Infinite-Space@users.noreply.github.com>
2022-11-12 17:43:30 -0500

    up deps

--- posts/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite"
-export default defineConfig({
-  ssr: {
-    noExternal: ["@giscus/vue"],
-    // error with top level import
-    // workaround from https://github.com/vuepress/vuepress-next/issues/585
-    // alternative solution https://github.com/vuejs/vitepress/issues/1579
-  },
-})
